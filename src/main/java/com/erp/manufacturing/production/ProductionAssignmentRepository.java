f39d586aab7fae9e38cadbd212939ea39eff9968
samudi-alwis <samudihirushika@gmail.com>
2026-07-06 00:35:09 +0530

    add ProductionAssignmentRepository

--- src/main/java/com/erp/manufacturing/production/ProductionAssignmentRepository.java
+++ src/main/java/com/erp/manufacturing/production/ProductionAssignmentRepository.java
@@ -3,5 +3,6 @@ package com.erp.manufacturing.production;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductionAssignmentRepository extends JpaRepository<ProductionAssignment, Long> {
 }
