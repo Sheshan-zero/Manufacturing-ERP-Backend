6cfe69a0a72ac475785e2987c8c2fa662c4eee6c
Tharindu Bandara <grenditz@gmail.com>
2026-07-07 12:42:55 +0530

    add Item repository

--- src/main/java/com/erp/manufacturing/item/ItemRepository.java
+++ src/main/java/com/erp/manufacturing/item/ItemRepository.java
@@ -3,5 +3,6 @@ package com.erp.manufacturing.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
